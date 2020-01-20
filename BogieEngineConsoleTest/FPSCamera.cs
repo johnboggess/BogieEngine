b0d4205e34a70731dd6ae31707100b1d18110f24
johnboggess <johnboggess@southern.edu>
2020-01-19 20:55:58 -0500

    added missing files

---
+++ BogieEngineConsoleTest/FPSCamera.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BogieEngineCore.Nodes;
+
+using OpenTK.Input;
+using OpenTK;
+
+namespace BogieEngineConsoleTest
+{
+    class FPSCamera : Camera
+    {
+        float moveScale = .1f;
+
+        bool initMouse = true;
+        float lastX = 0f;
+        float lastY = 0f;
+
+        float yawScale = 0.01f;
+        float pitchScale = 0.01f;
+
+        public override void Process()
+        {
+            KeyboardState ks = Keyboard.GetState();
+            MouseState ms = Mouse.GetCursorState();
+
+            if(initMouse)
+            {
+                initMouse = false;
+                lastX = ms.X;
+                lastY = ms.Y;
+            }
+
+            float diffX = ms.X - lastX;
+            float diffY = ms.Y - lastY;
+            lastX = ms.X;
+            lastY = ms.Y;
+            Console.WriteLine(diffX);
+
+            Transform.Rotate(Vector3.UnitY, diffX * yawScale);
+            Transform.Rotate(Transform.Right, diffY * pitchScale);
+
+            if (ks.IsKeyDown(Key.A))
+            {
+                Transform.Position -= Transform.Right * moveScale;
+            }
+            if (ks.IsKeyDown(Key.D))
+            {
+                Transform.Position += Transform.Right * moveScale;
+            }
+
+            if (ks.IsKeyDown(Key.W))
+            {
+                Transform.Position += Transform.Forwards * moveScale;
+            }
+            if (ks.IsKeyDown(Key.S))
+            {
+                Transform.Position -= Transform.Forwards * moveScale;
+            }
+
+            if (ks.IsKeyDown(Key.ShiftLeft))
+            {
+                Transform.Position += Vector3.UnitY * moveScale;
+            }
+            if (ks.IsKeyDown(Key.ControlLeft))
+            {
+                Transform.Position -= Vector3.UnitY * moveScale;
+            }
+        }
+    }
+}
---
+++ BogieEngineConsoleTest/Game.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+using BogieEngineCore;
+using BogieEngineCore.Shading;
+using BogieEngineCore.Nodes;
+using BogieEngineCore.Modelling;
+using BogieEngineCore.Texturing;
+namespace BogieEngineConsoleTest
+{
+    class Game : GameWindow
+    {
+        public ContentManager ContentManager;
+        public Color4 ClearColor = Color4.CornflowerBlue;
+        public Shader DefaultShader;
+        public Shader MaskCubeShader;
+
+        public Camera ActiveCamera = new FPSCamera();
+        public Root World = new Root();
+
+        public ModelNode _Samus;
+        public ModelNode _SamusNoVisor;
+        public ModelNode _Cube;
+
+
+        public Game(int width, int height, string title, int updateRate = 30, int frameRate = 30) : base(width, height, OpenTK.Graphics.GraphicsMode.Default, title)
+        {
+            ContentManager = new ContentManager();
+            Run(updateRate, frameRate);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(ClearColor);
+
+            ActiveCamera.Transform.Position = new Vector3(0, 0, 3);
+            ActiveCamera.Transform.Scale(new Vector3(1, 1, 1));
+
+            DefaultShader = new Shader("Resources/Shaders/default.vert", "Resources/Shaders/default.frag");
+            MaskCubeShader = new Shader("Resources/Shaders/default.vert", "Resources/Shaders/maskCube.frag");
+
+            //downloaded from https://sketchfab.com/3d-models/varia-suit-79c802129f9a4945aba62a607892ac31
+            _Samus = new ModelNode(ContentManager.LoadModel("Resources/Models/VariaSuit/DolBarriersuit.obj", DefaultShader));
+            _Samus.Transform.Scale(new Vector3(.1f, .1f, .1f));
+            _Samus.Transform.Position = new Vector3(-.5f, -1, 0);
+
+            _SamusNoVisor = new ModelNode(ContentManager.LoadModel("Resources/Models/VariaSuit/DolBarriersuit.obj", DefaultShader));
+            _SamusNoVisor.Transform.Scale(new Vector3(.1f, .1f, .1f));
+            _SamusNoVisor.Transform.Position = new Vector3(.5f, -1, 0);
+
+            World.AddNode(_Samus);
+            World.AddNode(_SamusNoVisor);
+
+            Texture cube0Tex = ContentManager.LoadTexture("Resources/Textures/Brick.jpg", TextureUnit.Texture0);
+            Texture cube1Tex = ContentManager.LoadTexture("Resources/Textures/Circle.png", TextureUnit.Texture1);
+
+            _Cube = new ModelNode(ContentManager.LoadModel("Resources/Models/Cube.obj", MaskCubeShader));
+            _Cube.Transform.Scale(new Vector3(3.5f, 3.5f, 1));
+            _Cube.Transform.Position = new Vector3(0, 0, -2);
+            _Cube.Model.MeshData[0].Textures.Add(cube0Tex);
+            _Cube.Model.MeshData[0].Textures.Add(cube1Tex);
+
+            World.AddNode(_Cube);
+            World.AddNode(ActiveCamera);
+
+            List<MeshData> meshData = _SamusNoVisor.GetMeshWithName("polygon6");
+            if (meshData.Count > 0) { meshData[0].Visible = false; }
+            base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            World.Process();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            DefaultShader.Projection = ActiveCamera.Projection;
+            DefaultShader.View = ActiveCamera.View;
+
+            MaskCubeShader.Projection = ActiveCamera.Projection;
+            MaskCubeShader.View = ActiveCamera.View;
+
+            World.Draw();
+
+            _Samus.Transform.Rotate(_SamusNoVisor.Transform.Up, -.01f);
+            _SamusNoVisor.Transform.Rotate(_SamusNoVisor.Transform.Up, -.01f);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            DefaultShader.Dispose();
+        }
+    }
+}
